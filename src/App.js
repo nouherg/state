0ac831235bbb92ed03b8bdb6a8001a2367727171
nouherg <105586185+nouherg@users.noreply.github.com>
2022-08-10 11:35:40 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 
 export default class App extends Component {
-  state = { fullname:" lana del rey",
+   constructor () {
+    super ();
+  this.state = { fullname:" lana del rey",
              bio :"singer",
              image:"https://th.bing.com/th/id/R.d728318534126f0ec2726c13169cee0f?rik=bsPn54BHirl3qQ&riu=http%3a%2f%2fpicture-cdn.wheretoget.it%2f4miru5-l-610x610-hat-lana%2bdel%2brey-pretty-fashion-sun%2bhat.jpg&ehk=teIoQl9kEUAJy5qKDGE0WQJ712NuqTqu%2bwkA5RMYFkk%3d&risl=&pid=ImgRaw&r=0",
              show:true,
-}
+}}
 handleClick = () => {
   this.setState({ show: !this.state.show });
 };
